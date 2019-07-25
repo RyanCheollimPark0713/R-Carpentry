814319cdee693178085aa1573ec0d6b75eaa9738
Ryan Park <park0713@hotmail.com>
2019-07-25 15:37:11 +1000

    Assign value to area_hectares

--- scripts/Introduction.R
+++ scripts/Introduction.R
@@ -4,3 +4,15 @@ library(tidyverse)
 
 print("Hello, World")
 
+(area_hectares <- 1.0)
+1.0 -> area_hectares
+
+area_hectares <- 2.5               # land area in hectares
+area_acres <- 2.47 * area_hectares # convert to acres
+area_acres                         # print
+
+length <- 2.5
+width <- 3.2
+area <- length * width
+area
+
