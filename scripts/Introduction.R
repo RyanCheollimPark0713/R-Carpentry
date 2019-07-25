2c6d03c7594d67eeda39598e0ad57691ed4e98d9
Ryan Park <park0713@hotmail.com>
2019-07-25 16:06:52 +1000

    vectors

--- scripts/Introduction.R
+++ scripts/Introduction.R
@@ -22,16 +22,39 @@ round(3.14159)
 round(3.14159, digits = 2)
 round(digits = 2, x = 3.14159)
 
+no_of_members <- c(3, 7, 10, 6)
+length(no_of_members)
+class(no_of_members)
+str(no_of_members)
 
+respondent_wall_type <- c("muddaub", "burntbricks", "sunbricks")
+length(respondent_wall_type)
+class(respondent_wall_type)
+str(respondent_wall_type)
 
+possessions <- c("bicycle", "radio", "television")
+possessions <- c(possessions, "mobile_phone")
+possessions <- c("car", possessions)
 
+c(TRUE, TRUE, FALSE)
+typeof(possessions)
 
+num_char <- c(1, 2, 3, "a")
+typeof(num_char)
+num_char
 
+num_logical <- c(1, 2, 3, FALSE)
+typeof(num_logical)
+num_logical
 
+char_logical <- c("a", "b", "c", TRUE)
+typeof(char_logical)
+char_logical
 
+tricky <- c(1, 2, 3, "4")
+tricky
 
-
-
-
+combined_logical <- c(num_logical, char_logical)
+combined_logical
 
 
