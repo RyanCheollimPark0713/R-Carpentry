2941a5b7b4046c7fcdde3647d243a1523eacd200
Ryan Park <park0713@hotmail.com>
2019-07-26 15:01:29 +1000

    spread and gather

--- scripts/dplyr.R
+++ scripts/dplyr.R
@@ -19,31 +19,59 @@ interviews %>%
   filter(memb_assoc == "yes") %>% 
   select(affect_conflicts, liv_count, no_meals) -> memb_assoc_data
 
-
 Interviews_ppl_room <- interviews %>%
   filter(memb_assoc == "yes") %>%
-  mutate(people_per_room = no_membrs / rooms)     # create a new column
+  mutate(people_per_room = no_membrs * no_meals)     # create a new column
 
-Total_meals <- interviews %>%
-  mutate(total_meals = no_membrs * rooms) %>%
-  filter(total_meals > 5) %>%
+total_meals <- interviews %>%
+  mutate(total_meals = no_membrs * no_meals) %>%     # create a new column
+  filter(total_meals > 20) %>%
   select(village, total_meals)
 
 
 
+interviews %>%
+  group_by(village) %>%
+  summarise(mean_no_membrs = mean(no_membrs))
+
+interviews %>%
+  group_by(village, memb_assoc) %>%
+  summarise(mean_no_membrs = mean(no_membrs))
+
+interviews %>%
+  group_by(village, memb_assoc) %>%
+  summarise(mean_no_membrs = mean(no_membrs), min_no_membrs = min(no_membrs))
+
+interviews %>%
+  filter(!is.na(memb_assoc)) %>%
+  group_by(village, memb_assoc) %>%
+  summarise(mean_no_membrs = mean(no_membrs), min_no_membrs = min(no_membrs)) %>%
+  arrange(desc(mean_no_membrs))            # descending order
+
+interviews %>% count(village, sort = TRUE) # descending order
+
+# Exercise
+interviews %>%
+  group_by(village) %>%
+  summarise(
+    mean = mean(no_membrs), 
+    max = max(no_membrs), 
+    min = min(no_membrs),
+    n = n()
+  )
 
 
+# Spread and Gather
+interviews_spread <- interviews %>%
+  mutate(wall_type_logical = TRUE) %>%
+  spread(key = respondent_wall_type, value = wall_type_logical, fill = FALSE)
 
 
-
-
-
-
-
-
-
-
-
+interviews_gather <- interviews_spread %>%
+  gather(key = "respondent_wall_type", value = "wall_type_logical",
+         burntbricks:sunbricks) %>%
+  filter(wall_type_logical == TRUE) %>%
+  select(-wall_type_logical)
 
 
 
